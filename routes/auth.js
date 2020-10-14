2f906f1a89385310c07ef1623532d259347dd3b8
gjbonner <gjbonner@gmail.com>
2020-10-14 16:51:27 -0400

    login route done | add JWT token

--- routes/auth.js
+++ routes/auth.js
@@ -1,40 +1,60 @@
-const { Router } = require('express');
-const User = require('../models/User');
-const router = require('express').Router();
-const bcrypt = require('bcryptjs');
-const { registerValidation } = require('../validation');
+const { Router } = require("express");
+const User = require("../models/User");
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { registerValidation, loginValidation } = require("../validation");
 
-router.post('/register', async (req, res) => {
-    // validate user 
-    const {error} = registerValidation(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
-    
-    // check if user exists
-    const emailExists = await User.findOne({email: req.body.email});
-    if(emailExists) return res.status(400).send('Account with that email already exists');
-    
-    // Hash password
-    const salt = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(req.body.password, salt);
+router.post("/register", async (req, res) => {
+  // validate user
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    // Create user 
-    const user = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: hashPassword,
-    })
-    try {
-        const savedUser = await user.save();
-        res.status(res.statusCode).send(savedUser);
-    } catch (error) {
-        res.status(res.statusCode).send(error);
-    }
+  // check if user exists
+  const emailExists = await User.findOne({ email: req.body.email });
+  const usernameExists = await User.findOne({ username: req.body.username });
+  if (emailExists) {
+    return res.status(400).send("Account with that email already exists");
+  } else if (usernameExists) {
+    return res.status(400).send("Account with username already exists");
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+  // Create user
+  const user = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: hashPassword,
+  });
+  try {
+    const savedUser = await user.save();
+    res.status(res.statusCode).send({ user: user._id });
+  } catch (error) {
+    res.status(res.statusCode).send(error);
+  }
 });
 
-router.post('/login', (req, res) => {
-    res.send('Login');
-})
+router.post("/login", async (req, res) => {
+  // validate user
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
+  // check if user exists
+  const user = await User.findOne({ username: req.body.username });
+  if (!user) {
+    return res.status(400).send("Invalid email or password.");
+  }
 
+  //Check password
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send("Invald email or password.");
+
+  //create JWT Token
+  const token = jwt.sign({ _id: user.id }, process.env.SECRET_TOKEN);
+  res.header("auth-token", token).send(token);
+});
 
 module.exports = router;
--- validation.js
+++ validation.js
@@ -1,22 +1,22 @@
 // VALIDATION
-const Joi = require('@hapi/joi');
+const Joi = require("@hapi/joi");
 
 const registerValidation = (data) => {
-    const schema = Joi.object({
-        username: Joi.string().min(4).required(),
-        password: Joi.string().min(5).required(),
-        email: Joi.string().email({ minDomainSegments: 2 })
-    })
-    return schema.validate(data);
-}
+  const schema = Joi.object({
+    username: Joi.string().min(4).required(),
+    password: Joi.string().min(5).required(),
+    email: Joi.string().email({ minDomainSegments: 2 }),
+  });
+  return schema.validate(data);
+};
 
 const loginValidation = (data) => {
-    const schema = Joi.object({
-        username: Joi.string().min(4).required(),
-        password: Joi.string().min(5).required(),
-    })
-    return schema.validate(data);
-}
-
+  const schema = Joi.object({
+    username: Joi.string().min(4).required(),
+    password: Joi.string().min(5).required(),
+  });
+  return schema.validate(data);
+};
 
 module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
